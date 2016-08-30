ddb32a471ca437a6ad1ccd2fc2b17c5378a95026
pramayu <gedepramayu@gmail.com>
2016-08-30 23:40:58 +0700

    event feature

--- js/main.js
+++ js/main.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
 	$('#slide_home').carousel({
 	  interval: 1000 * 10
 	});
+	$('#letr').carousel({
+	  interval: 1000 * 100
+	});
+});
+$(function(){
+	jQuery("html").niceScroll({cursorcolor:"#c0bfbf"});
 });
